e43ea9bbe3efa90373255a37512894e00ecc783a
Nicholas Chew <chewonithard@gmail.com>
2021-11-20 17:51:27 +0800

    backend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require 'json'
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=4eb5d4ec06930b8f7ae05e34b041348b"
+
+data = JSON.parse(URI.open(url).read)
+
+data["results"].each do |movie|
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: movie["poster_path"], rating: movie["vote_average"].to_f)
+end
