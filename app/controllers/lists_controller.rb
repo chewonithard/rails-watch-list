cf8ff5f3728510000e069b5c9d3b76fefb881c58
Nicholas Chew <chewonithard@gmail.com>
2021-11-22 21:41:21 +0800

    pushed to heroku

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,10 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    if @list.save
-      redirect_to @list
-    else
-      render :new
-    end
+    puts 'test'
+    puts @list
+    @list.save
+    redirect_to @list
   end
 
   def show
@@ -21,6 +20,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
